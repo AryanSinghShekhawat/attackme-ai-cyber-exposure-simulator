da224835029b402d4b881d939f63748ed825a9ce
Aryan <aryansinghshekhawat851@gmail.com>
2026-02-20 19:36:42 +0530

    Switched to Streamlit secrets for deployment

--- attack_engine.py
+++ attack_engine.py
@@ -1,10 +1,9 @@
 from openai import OpenAI
-import os
-from dotenv import load_dotenv
+import streamlit as st
 
-load_dotenv()
+# Use Streamlit Secrets (NOT dotenv)
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def generate_attack_profile(user_summary):
 
