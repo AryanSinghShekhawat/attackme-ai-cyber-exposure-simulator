ec85c3581d728db4353c8e0078a1269252d90700
Aryan <aryansinghshekhawat851@gmail.com>
2026-02-21 06:45:42 +0530

    Add responsive background UI

--- core/attack_graph.py
+++ core/attack_graph.py
@@ -1,18 +1,19 @@
 # core/attack_graph.py
 
 def generate_attack_paths(profile):
+
     paths = {
         "Phishing → Credential Theft → Account Takeover":
-            profile["phishing"] * profile["credential_reuse"],
+            profile["phishing_risk"] * profile["credential_reuse"],
 
         "Public Info → Social Engineering → Fraud":
-            profile["public_exposure"] * profile["phishing"],
+            profile["public_exposure"] * profile["phishing_risk"],
 
         "Device Exploit → Credential Dump":
             profile["device_security"] * profile["credential_reuse"],
 
         "Combined Multi-Vector Attack":
-            profile["phishing"] *
+            profile["phishing_risk"] *
             profile["credential_reuse"] *
             profile["public_exposure"]
     }
